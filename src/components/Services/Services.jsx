ea6f0f8b4012340ba68ccec33ae439e0af7bf852
Ahmad Habib <ahmadalkali69@gmail.com>
2022-04-21 11:03:05 +0100

    Services section complete

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,6 @@
 import { Grid, Typography, useMediaQuery } from '@mui/material'
 import { Box } from '@mui/system'
-import bookRides from '../../assets/bookRides.png'
-import earnPoints from '../../assets/earnPoints.svg'
-import sendDeliveries from '../../assets/sendDeliveries.png'
-import abstract from '../../assets/services/abstract.svg'
+// import abstract from '../../assets/services/abstract.svg'
 import circle from '../../assets/services/circle.svg'
 import circle2 from '../../assets/services/circle2.svg'
 import circle3 from '../../assets/services/circle3.svg'
@@ -21,155 +18,116 @@ const Services = () => {
 
   return (
     <Box id="services" className={classes.services}>
-      <img src={abstract} alt="Abstract art" className={classes.abstract} />
-      <Typography variant="h2" data-aos="fade-up">
-        Our Services
-      </Typography>
-      <Typography
-        className={classes.text}
-        data-aos="fade-up"
-        data-aos-delay="100"
-      >
-        We provide convenient, safe and secure keke rides. With the aid of our
-        registered, civilized and advanced fasta{' '}
-        <span style={{ color: '#00AEE8' }}>
-          <span style={{ fontWeight: 700 }}>bosses</span> (riders)
-        </span>
-        , for our
-        <span style={{ color: '#00AEE8' }}>
-          <span style={{ fontWeight: 700 }}> guests</span> (passengers)
-        </span>
-        . Earn points for every trip that you take, which you can use to book
-        for other trips or donate to any charity foundation of your choice.
-      </Typography>
+      {/* <img src={abstract} alt="Abstract art" className={classes.abstract} /> */}
 
-      <Grid
-        id="bookRide"
-        mt={{ xs: 10, sm: 0 }}
-        container
-        sx={{ position: 'relative' }}
-      >
-        <Grid item xs={12} sm={6} data-aos="fade-right">
-          <Box className={classes.feature}>
+      <Grid id="bookRide" container sx={{ position: 'relative' }}>
+        <Grid item xs={6} data-aos="fade-right">
+          <Box
+            className={classes.feature}
+            sx={{
+              justifyContent: 'flex-end',
+              paddingTop: isDesktop ? 0 : '50px'
+            }}
+          >
             <Box
               className={classes.featureImg}
               sx={{ backgroundColor: '#1493C917' }}
-            >
-              <img src={bookRides} alt="Book rides" style={{ width: 60 }} />
-            </Box>
+            />
             <Box>
               <Typography className={classes.featureTxt1}>
-                Book rides
+                Fastest QR Code Payment
               </Typography>
               <Typography className={classes.featureTxt2}>
-                Easily book keke rides to your destination
+                Send or receive money easily by scanning QR code
               </Typography>
             </Box>
           </Box>
-          <Typography className={classes.featureTxt3}>
-            <span style={{ color: '#00AEE8' }}>Request a ride</span>, meet our
-            boss, enjoy the journey, get to your destinsation
-          </Typography>
         </Grid>
 
         <Grid
           mt={{ xs: 5, sm: 0 }}
           item
-          xs={12}
-          sm={6}
+          xs={6}
           className={classes.svg}
           sx={{ marginTop: 2 }}
           data-aos="fade-left"
         >
           <img
-            src={isDesktop ? iPhone : iPhoneMobile}
+            src={iPhone}
             alt="iPhone"
-            style={isDesktop ? null : { width: 300 }}
+            style={
+              isDesktop
+                ? { marginRight: -200 }
+                : { width: '150%', marginRight: '-130px' }
+            }
           />
         </Grid>
         <img src={circle} alt="circle" className={classes.circle} />
-        <img src={circle2} alt="circle2" className={classes.circle2} />
       </Grid>
 
       <Grid
         id="deliveries"
         container
         sx={{ display: 'flex', alignItems: 'center' }}
-        mt={{ xs: 10, md: -10 }}
+        mt={{ xs: 5, md: -40 }}
         className={classes.wrapReverse}
       >
-        <Grid item xs={12} sm={6} className={classes.svg}>
+        <Grid item xs={6} className={classes.svg}>
           <img
-            src={isDesktop ? iPhoneWithHand : iPhoneWithHandMobile}
+            src={iPhoneWithHand}
             alt="iPhone with hand"
-            style={isDesktop ? null : { width: 350 }}
+            style={isDesktop ? null : { width: '100%' }}
             data-aos="fade-right"
           />
         </Grid>
 
-        <Grid item xs={12} sm={6} data-aos="fade-left">
-          <Box className={classes.feature}>
+        <Grid item xs={6} data-aos="fade-left">
+          <Box
+            className={classes.feature}
+            sx={{ marginTop: isDesktop ? '300px' : 0 }}
+          >
             <Box
               style={{ backgroundColor: '#eaf5fa', marginRight: 15 }}
               className={classes.featureImg}
-            >
-              <img
-                src={sendDeliveries}
-                alt="Send Deliveries"
-                style={{ width: 60 }}
-              />
-            </Box>
+            />
             <Box>
               <Typography className={classes.featureTxt1}>
-                Send Deliveries
+                Earn points for every transaction you make
               </Typography>
               <Typography className={classes.featureTxt2}>
-                Easily send deliveries to any location of your choice
+                The more you spend, the more points you earn
               </Typography>
             </Box>
           </Box>
-          <Typography className={classes.featureTxt3}>
-            <span style={{ color: '#00AEE8' }}>Request for a delivery</span>,
-            meet our boss and get your package delivered in no time
-          </Typography>
         </Grid>
-        <img src={circle3} alt="" className={classes.circle3} />
       </Grid>
 
       <Grid
         id="earnPoint"
         container
+        mt={{ xs: 5 }}
         sx={{ display: 'flex', alignItems: 'center' }}
       >
-        <Grid item xs={12} sm={6} data-aos="fade-right">
+        <Grid item xs={6} data-aos="fade-right">
           <Box className={classes.feature}>
-            <img
-              src={earnPoints}
-              alt="Earn points"
-              style={{ marginRight: 15, width: 83 }}
+            <Box
+              style={{ backgroundColor: '#eaf5fa', marginRight: 15 }}
+              className={classes.featureImg}
             />
             <Box>
               <Typography className={classes.featureTxt1}>
-                Earn Points
+                Easily redeem points
               </Typography>
               <Typography className={classes.featureTxt2}>
-                Easily earn points for every ride you take
+                Reseem points and use as payment or donate to local charity of
+                your choice
               </Typography>
             </Box>
           </Box>
-          <Typography className={classes.featureTxt3}>
-            <span style={{ color: '#00AEE8' }}>You earn points </span>
-            after every ride which you can either use to book for another ride,
-            or donate to any charity foundation of your choice
-          </Typography>
         </Grid>
 
-        <Grid item xs={12} sm={6} className={classes.svg} data-aos="fade-left">
-          <img
-            src={money}
-            alt="Money"
-            style={{ width: 'clamp(300px, 40vw, 450px)' }}
-          />
+        <Grid item xs={6} className={classes.svg} data-aos="fade-left">
+          <img src={money} alt="Money" style={{ width: '100%' }} />
         </Grid>
       </Grid>
     </Box>
--- src/components/Services/useStyles.js
+++ src/components/Services/useStyles.js
@@ -1,114 +1,120 @@
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles((theme) => ({
-	services: {
-		padding: '10vw 5vw',
-		display: 'grid',
-		placeItems: 'center',
-		position: 'relative'
-	},
-	text: {
-		'&.MuiTypography-root': {
-			fontSize: 'clamp(1rem, 2vw, 18px)',
-			width: '80%',
-			textAlign: 'center',
-			fontWeight: 300,
-			margin: '3vw 0',
-			lineHeight: '30px',
-			marginBottom: theme.spacing(20)
-		},
+  services: {
+    padding: '10vw 5vw',
+    display: 'grid',
+    placeItems: 'center',
+    position: 'relative'
+  },
+  text: {
+    '&.MuiTypography-root': {
+      fontSize: 'clamp(1rem, 2vw, 18px)',
+      width: '80%',
+      textAlign: 'center',
+      fontWeight: 300,
+      margin: '3vw 0',
+      lineHeight: '30px',
+      marginBottom: theme.spacing(20)
+    },
 
-		[theme.breakpoints.down('sm')]: {
-			'&.MuiTypography-root': {
-				width: '100%',
-				lineHeight: '24px',
-				marginBottom: theme.spacing(5)
-			}
-		}
-	},
-	wrapReverse: {
-		[theme.breakpoints.down('md')]: {
-			'&.MuiGrid-root': {
-				flexWrap: 'wrap-reverse'
-			}
-		}
-	},
-	svg: {
-		display: 'flex',
-		justifyContent: 'center'
-	},
-	feature: {
-		display: 'flex',
-		alignItems: 'center'
-	},
-	featureImg: {
-		minWidth: 83,
-		height: 83,
-		display: 'grid',
-		placeItems: 'center',
-		borderRadius: '50%',
-		marginRight: '15px'
-	},
-	featureTxt1: {
-		'&.MuiTypography-root': {
-			fontSize: '17px',
-			fontWeight: 500
-		}
-	},
-	featureTxt2: {
-		'&.MuiTypography-root': {
-			fontSize: '12px'
-		}
-	},
-	featureTxt3: {
-		'&.MuiTypography-root': {
-			fontSize: 'clamp(18px, 2vw, 24px)',
-			fontWeight: 600,
-			paddingLeft: theme.spacing(5),
-			marginTop: theme.spacing(3)
-		},
+    [theme.breakpoints.down('sm')]: {
+      '&.MuiTypography-root': {
+        width: '100%',
+        lineHeight: '24px',
+        marginBottom: theme.spacing(5)
+      }
+    }
+  },
+  wrapReverse: {
+    [theme.breakpoints.down('md')]: {
+      '&.MuiGrid-root': {
+        flexWrap: 'wrap-reverse'
+      }
+    }
+  },
+  svg: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  feature: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  featureImg: {
+    minWidth: 83,
+    height: 83,
+    display: 'grid',
+    placeItems: 'center',
+    borderRadius: '50%',
+    marginRight: '15px',
 
-		[theme.breakpoints.down('sm')]: {
-			'&.MuiTypography-root': {
-				paddingLeft: 0
-			}
-		}
-	},
-	circle: {
-		position: 'absolute',
-		right: 0,
-		marginRight: '-5vw',
-		zIndex: -100,
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    }
+  },
+  featureTxt1: {
+    '&.MuiTypography-root': {
+      fontSize: '1.6rem',
+      fontWeight: 600,
+      marginBottom: theme.spacing()
+    },
 
-		[theme.breakpoints.down('sm')]: {
-			display: 'none'
-		}
-	},
-	circle2: {
-		display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      '&.MuiTypography-root': {
+        fontSize: '1.2rem'
+      }
+    }
+  },
+  featureTxt2: {
+    '&.MuiTypography-root': {
+      fontSize: '1.2rem'
+    },
 
-		[theme.breakpoints.down('sm')]: {
-			display: 'flex',
-			position: 'absolute',
-			top: 120,
-			left: 0,
-			marginLeft: '-5vw',
-			zIndex: -100
-		}
-	},
-	circle3: {
-		position: 'absolute',
-		left: 0,
-		zIndex: -100
-	},
-	abstract: {
-		position: 'absolute',
-		opacity: 0.05,
-		top: '20vw',
-		width: 'clamp(200px, 40vw, 650px)',
-		left: '15vw',
-		zIndex: -200
-	}
+    [theme.breakpoints.down('sm')]: {
+      '&.MuiTypography-root': {
+        fontSize: '1rem'
+      }
+    }
+  },
+  featureTxt3: {
+    '&.MuiTypography-root': {
+      fontSize: 'clamp(18px, 2vw, 24px)',
+      fontWeight: 600,
+      paddingLeft: theme.spacing(5),
+      marginTop: theme.spacing(3)
+    },
+
+    [theme.breakpoints.down('sm')]: {
+      '&.MuiTypography-root': {
+        paddingLeft: 0
+      }
+    }
+  },
+  circle: {
+    position: 'absolute',
+    top: '50%',
+    right: 0,
+    marginRight: '-5vw',
+    zIndex: -100,
+
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    }
+  },
+  circle3: {
+    position: 'absolute',
+    left: 0,
+    zIndex: -100
+  },
+  abstract: {
+    position: 'absolute',
+    // opacity: 0.2,
+    top: '20vw',
+    width: 'clamp(200px, 40vw, 650px)',
+    left: '15vw',
+    zIndex: -200
+  }
 }))
 
 export default useStyles
